8f2de884a5d814829b5a20a379f44b3c23f37cd8
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-01-11 09:27:04 +0200

    middleware basics note comment add

--- 02-express-tutorial/final/08-middleware-basic.js
+++ 02-express-tutorial/final/08-middleware-basic.js
@@ -1,23 +1,23 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 //  req => middleware => res
 
 const logger = (req, res, next) => {
-  const method = req.method
-  const url = req.url
-  const time = new Date().getFullYear()
-  console.log(method, url, time)
-  next()
-}
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().getFullYear();
+  console.log(method, url, time);
+  next(); // next() is neccessary to pass onto the next middleware (prevents infinite page loading)
+};
 
-app.get('/', logger, (req, res) => {
-  res.send('Home')
-})
-app.get('/about', logger, (req, res) => {
-  res.send('About')
-})
+app.get("/", logger, (req, res) => {
+  res.send("Home");
+});
+app.get("/about", logger, (req, res) => {
+  res.send("About");
+});
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
-})
+  console.log("Server is listening on port 5000....");
+});
