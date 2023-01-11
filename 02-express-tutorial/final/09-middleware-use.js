5a626b259769b2654f36332017a92ad0dadcbb09
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-01-11 09:45:59 +0200

    minor note add

--- 02-express-tutorial/final/09-middleware-use.js
+++ 02-express-tutorial/final/09-middleware-use.js
@@ -1,24 +1,27 @@
-const express = require('express')
-const app = express()
-const logger = require('./logger')
-const authorize = require('./authorize')
+const express = require("express");
+const app = express();
+
+const logger = require("./logger"); // import middleware modules intp project
+const authorize = require("./authorize"); // import middleware modules intp project
+
 //  req => middleware => res
-app.use([logger, authorize])
+app.use([logger, authorize]);
+
 // api/home/about/products
-app.get('/', (req, res) => {
-  res.send('Home')
-})
-app.get('/about', (req, res) => {
-  res.send('About')
-})
-app.get('/api/products', (req, res) => {
-  res.send('Products')
-})
-app.get('/api/items', (req, res) => {
-  console.log(req.user)
-  res.send('Items')
-})
+app.get("/", (req, res) => {
+  res.send("Home");
+});
+app.get("/about", (req, res) => {
+  res.send("About");
+});
+app.get("/api/products", (req, res) => {
+  res.send("Products");
+});
+app.get("/api/items", (req, res) => {
+  console.log(req.user);
+  res.send("Items");
+});
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
-})
+  console.log("Server is listening on port 5000....");
+});
