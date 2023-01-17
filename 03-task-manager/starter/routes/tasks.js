f8ad57aba4cbab878c3d1dc79666c745817e6c1a
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-01-17 10:44:59 +0200

    TM bug fix

--- 03-task-manager/starter/routes/tasks.js
+++ 03-task-manager/starter/routes/tasks.js
@@ -1,2 +1,8 @@
 const express = require("express");
-const routers = express.Router();
+const router = express.Router();
+
+const { getAllTasks } = require("../controllers/tasks");
+
+router.route("/").get(getAllTasks);
+
+module.exports = router;
