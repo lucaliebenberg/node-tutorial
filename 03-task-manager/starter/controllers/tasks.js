575f41f191fb00396768f698128341b93904a8cb
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-01-17 21:05:47 +0200

    controller file try/catch add

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,19 +1,31 @@
 const Task = require("../models/Task");
 
-const getAllTasks = (req, res) => {
-  res.send("get all tasks");
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: "There was an error :(" });
+  }
 };
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: "There was an error :(" });
+  }
 };
+
 const getTask = (req, res) => {
   res.json({ id: req.params.id });
 };
+
 const updateTask = (req, res) => {
   res.send("update task");
 };
+
 const deleteTask = (req, res) => {
   res.send("delete task");
 };
