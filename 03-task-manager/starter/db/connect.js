9215af4b877366cb17000f363243299e3246a181
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-01-17 19:38:38 +0200

    Db connect setup complete

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,2 +1,9 @@
+const mongoose = require("mongoose");
+
 const connectionString =
-  "mongodb+srv://luca:<password>@cluster0.sn9hl87.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://luca:milo060701@cluster0.sn9hl87.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority";
+
+mongoose
+  .connect(connectionString)
+  .then(() => console.log("Connected to the DB"))
+  .catch((err) => console.log(err));
