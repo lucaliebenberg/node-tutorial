38e251d63221abe76587662fd9ca4c98fb8b0338
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-01-20 20:02:19 +0200

    populate data connected with db

--- 04-store-api/starter/populate.js
+++ 04-store-api/starter/populate.js
@@ -5,12 +5,16 @@ const Product = require("./models/product");
 
 const jsonProducts = require("./products.json");
 
-const start = async (req, res) => {
+const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
+    await Product.deleteMany();
+    await Product.create(jsonProducts);
     console.log("success");
+    process.exit(0);
   } catch (error) {
     console.log(error);
+    process.exit(1);
   }
 };
 
