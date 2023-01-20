36f7f6231bbe3bc5f2ced0745d649a4bcd34deff
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-01-20 19:52:13 +0200

    populate.js file commit

--- 04-store-api/starter/populate.js
+++ 04-store-api/starter/populate.js
@@ -4,3 +4,14 @@ const connectDB = require("./db/connect");
 const Product = require("./models/product");
 
 const jsonProducts = require("./products.json");
+
+const start = async (req, res) => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    console.log("success");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
