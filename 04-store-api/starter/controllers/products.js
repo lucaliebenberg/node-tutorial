dfc57ce79a6154bbb6a87f5292457b1d10dd2692
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-01-20 20:42:04 +0200

    products featured add

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,10 +1,20 @@
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-  throw new Error("Not implemented async error");
-  res.status(200).json({ msg: "products testing route" });
+  const products = await Product.find({ featured: true });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
-const getAllProducts = (req, res) => {
-  res.status(200).json({ msg: "products route" });
+const getAllProducts = async (req, res) => {
+  const { featured } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  const products = await Product.find(req.query);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
